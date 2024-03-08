9beb2b60fd027c3a77dedd3990f2fb1f661ad3e8
D-I-V-Y-A-S <divyaselvam1405@gmail.com>
2024-03-08 15:32:53 +0530

    Initial message

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UsersComponent from './UsersComponent/UsersComponent'
+
+const App = () => {
+  return (
+    <React.Fragment>
+    <UsersComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/UsersComponent/UsersComponent.jsx
@@ -0,0 +1,31 @@
+
+import axios from 'axios'
+import React, { useState } from 'react'
+
+const UsersComponent = () => {
+    const [Userdata,setUserData]=useState(null)
+    const getDataFromApi =async()=>{
+        // const response=await fetch("https://jsonplaceholder.typicode.com/users")
+        // const data=await response.json()
+        // console.log(data)
+        // setUserData(data)
+        const receivedData=await axios.get("https://jsonplaceholder.typicode.com/users")
+        console.log(receivedData)
+        setUserData(receivedData.data)
+    }
+  return (
+    <React.Fragment>
+    <h1>Users list</h1>
+<button onClick={getDataFromApi}>Get Users</button>
+<ul> 
+{Userdata && Userdata.map((iterator)=>(
+<li key={iterator.id}>
+    <p>iterator.name,iterator.address.city</p>
+    </li>
+    ))}
+</ul>
+    </React.Fragment>
+  )
+}
+
+export default UsersComponent
